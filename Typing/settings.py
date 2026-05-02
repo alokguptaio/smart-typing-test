a1d13e6b78d4b32fef493414ca806c983225b312
Alok Gupta <alokgupta482005@gmail.com>
2026-05-02 18:21:51 +0530

    allow host *

--- Typing/settings.py
+++ Typing/settings.py
@@ -19,7 +19,7 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 print("DON_ENV LOADED!")
 
-ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["*"]
 
 # ---------------- INSTALLED APPS ----------------
 INSTALLED_APPS = [
